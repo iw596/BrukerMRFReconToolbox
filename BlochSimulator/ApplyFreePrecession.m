54f04c00f619b8c24183a5700efd264128ec8fe7
iw596 <iw596@york.ac.uk>
2025-05-02 16:30:15 +0100

    Sim dev

---
+++ BlochSimulator/ApplyFreePrecession.m
@@ -0,0 +1,9 @@
+%% This function modles the free precession of a spin system with or without a gradient
+%% key parameters are M (3xN
+function [outputArg1,outputArg2] = ApplyFreePrecession(M,T1,T2,dt,varargin)
+%FREEPRECESS Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
--- BlochSimulator/freeprecess.m
+++
@@ -1,8 +0,0 @@
-%%
-function [outputArg1,outputArg2] = freeprecess(inputArg1,inputArg2)
-%FREEPRECESS Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
