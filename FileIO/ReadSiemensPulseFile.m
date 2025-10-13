f9310a8513070f40031bd622c8ace8e00bcdc582
iw596 <iw596@york.ac.uk>
2025-10-13 20:47:04 +0100

    Update ReadSiemensPulseFile.m

--- FileIO/ReadSiemensPulseFile.m
+++ FileIO/ReadSiemensPulseFile.m
@@ -1,7 +1,33 @@
-function res = ReadSiemensPulseFile(pth)
-%READSIEMENSPULSEFILE Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+%%% Function to read siemens PTA RF pulse structure. 
+%%% a PTA RF file is an ASCII strucure. So we read it in
+%%% and then parse out the header information we need 
+function res = ReadSiemensPulseFile(filepath)
+ 
+    opt = {'CollectOutput',true};
+    hdr = {};
+    out = {};
+    [fid,msg] = fopen(filepath,'rt');
+    assert(fid>=3,msg) % ensure the file opened correctly.
+    while ~feof(fid)
+        hdr{end+1} = fgetl(fid);
+        out(end+1) = textscan(fid,'%f%f',opt{:});
+    end
+    fclose(fid);
+    res.hdr = hdr;
+    out = out(~cellfun('isempty',out));
+    out = cell2mat(out);
+    amp = out(1:2:end);
+    phs = out(2:2:end);
+    res.out = amp.*exp(-1j.*phs);
 end
 
+function txt = get_char_in_block(fid, size)
+block  = char(fread(fid, size, 'char')');
+endpos = strfind(block,sprintf('\n'));
+txt    = block(1:endpos(1)-1); % stop at first nullchar
+end % fcn
+function txt = get_comment_block(fid, size)
+block  = char(fread(fid, size, 'char')');
+endpos = strfind(block,sprintf('\n'));
+txt    = block(1:endpos(end)-1); % stop at last nullchar in the block
+end % fcn
