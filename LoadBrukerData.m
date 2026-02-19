bf9a36c2e3a0c8822020bfda0ea380bbc005de81
iw596 <43037941+iw596@users.noreply.github.com>
2026-02-19 14:47:34 +0000

    i/o fix

--- LoadBrukerData.m
+++ LoadBrukerData.m
@@ -203,7 +203,7 @@ function params = LoadBrukerData(path,loadDataFlag)
         sliceSpoiler.duration = str2num(tmp{3})/1000; % Spoiler duration in s
         sliceSpoiler.NCycles = str2num(tmp{2});
         amp = cell2mat(tmp(4));
-        amp = amp(1:end-1);
+        %amp = amp(1:end-1);
         sliceSpoiler.amplitude = str2double(amp);
         params.sliceSpoiler = sliceSpoiler;
     end
